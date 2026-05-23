ec67dc0bfd7abe2042eea7000353568d9f920d8e
Jonathan Chávez <jonatahan.chavez1188@alumnos.udg.mx>
2026-05-22 23:50:56 -0600

    Update MenuPrincipal.php

--- Trapos/MenuPrincipal.php
+++ Trapos/MenuPrincipal.php
@@ -15,7 +15,7 @@ $usuario = $_SESSION['usuario'];
 <head>
     <meta charset="UTF-8">
     <title>Menú Principal | Mis Trapitos</title>
-    <link rel="icon" type="image/png" href="Imagenes/icono.png">
+    <link rel="icon" type="image/png" href="ImageL.png">
     <link rel="stylesheet" href="estilo/estilo.css">
 </head>
 <body>
@@ -41,12 +41,19 @@ $usuario = $_SESSION['usuario'];
                 </tr>
                 <tr>
                     <td><a href="inventario.php?=modificar" class="accion">Modificar Producto</a></td>
-                    <td><a href="inventario.php?=eliminar" class="accion">Dar de Baja</a></td>
+                    <td><a href="inventario.php?=eliminar" class="accion eliminar">Dar de Baja</a></td>
                 </tr>
                 <tr>
-                    <td colspan="2"><a href="inventario.php?seccion=gc" class="accion">Gestionar Categorías</a></td>
+                    <td colspan="2"><a href="productos_categorias.php" class="accion">Gestionar Categorías</a></td>
                 </tr>
-
+                <tr><th colspan="2">VENTAS Y CAJA</th></tr>
+                <tr>
+                    <td><a href="ventas_nueva.php" class="accion">Nueva Venta</a></td>
+                    <td><a href="inventario_consulta.php" class="accion">Consultar Disponibilidad</a></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><a href="promociones_lista.php?filtro=activas" class="accion">Ver Promociones Activas</a></td>
+                </tr>
                 <tr><th colspan="2">Reportes Y Sistema</th></tr>
                 <tr>
                     <td><a href="reporte_diario.php" class="accion">Reporte Diario</a></td>
@@ -66,7 +73,7 @@ $usuario = $_SESSION['usuario'];
                    
                 </tr>
                                 <tr><th colspan="2">Gestionar promociones</th></tr>
-                   <tr>
+                <tr>
                     <td><a href="promociones_alta.php" class="accion">Registrar Promoción</a></td>
                     <td><a href="promociones_lista.php?filtro=todas" class="accion">Ver Promociones</a></td>
                 </tr>
@@ -74,8 +81,19 @@ $usuario = $_SESSION['usuario'];
                     <td><a href="productos_modifica.php" class="accion">Modificar Producto</a></td>
                     <td><a href="productos_elimina.php" class="accion eliminar">Dar de Baja</a></td>
                 </tr>
+                  <tr><th colspan="2">PROVEEDORES</th></tr>
                 <tr>
-                    <td colspan="2"><a href="productos_categorias.php" class="accion">Gestionar Categorías</a></td>
+                    <td><a href="proveedores_alta.php" class="accion">Añadir Proveedores</a></td>
+                    <td><a href="proveedores_productos.php" class="accion">Productos por Proveedor</a></td>
+                </tr>
+                   <tr>
+                    <td><a href="proveedores_lista.php" class="accion">Directorio de Proveedores</a></td>
+        
+                </tr>
+                 <tr><th colspan="2">CLIENTES</th></tr>
+                <tr>
+                    <td><a href="clientes_alta.php" class="accion">Registrar Cliente</a></td>
+                    <td><a href="clientes_historial.php" class="accion">Historial de Compras</a></td>
                 </tr>
 
             </table>
@@ -90,7 +108,7 @@ $usuario = $_SESSION['usuario'];
                     <td><a href="inventario_consulta.php" class="accion">Consultar Disponibilidad</a></td>
                 </tr>
                 <tr>
-                    <td colspan="2"><a href="descuentos_ver.php" class="accion">Ver Promociones Activas</a></td>
+                    <td colspan="2"><a href="promociones_lista.php?filtro=activas" class="accion">Ver Promociones Activas</a></td>
                 </tr>
 
                 <tr><th colspan="2">CLIENTES</th></tr>
@@ -101,7 +119,7 @@ $usuario = $_SESSION['usuario'];
 
                 <tr><th colspan="2">PROVEEDORES</th></tr>
                 <tr>
-                    <td><a href="proveedores_mostrar.php" class="accion">Directorio de Proveedores</a></td>
+                    <td><a href="proveedores_lista.php" class="accion">Directorio de Proveedores</a></td>
                     <td><a href="proveedores_productos.php" class="accion">Productos por Proveedor</a></td>
                 </tr>
                                 <tr><th colspan="2">PRODUCTOS</th></tr>
@@ -117,10 +135,11 @@ $usuario = $_SESSION['usuario'];
                     <td colspan="2"><a href="inventario.php?seccion=gc" class="accion">Gestionar Categorías</a></td>
                 </tr>
                                 <tr><th colspan="2">DESCUENTOS</th></tr>
-              <tr>
+                <tr>
                     <td><a href="promociones_lista.php?filtro=todas" class="accion">Lista de promociones</a></td>
                 </tr>
-
+               
+                
 
 
             </table>
